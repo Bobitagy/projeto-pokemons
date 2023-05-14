c86c2dd0cb524ee3838ea41d620d84fc9e450b91
Henrique Souza <133029799+Bobitagy@users.noreply.github.com>
2023-05-14 00:21:47 -0300

    Add files via upload

---
+++ SRC/jv/indox.js
@@ -0,0 +1,15 @@
+const botaoAlterarTema = document.getElementById("botao-alterar-tema");
+const body = document.querySelector("body");
+const imagemBotaoTrocaDeTema = document.querySelector(".imagem-botom");
+
+botaoAlterarTema.addEventListener("click", () => {
+  const modoEscuroEstaAtivo = body.classList.contains("modo-escuro");
+
+  body.classList.toggle("modo-escuro");
+
+  if (modoEscuroEstaAtivo) {
+    imagemBotaoTrocaDeTema.setAttribute("src", "./SRC/imagens/sun.png");
+  } else {
+    imagemBotaoTrocaDeTema.setAttribute("src", "./SRC/imagens/moon.png");
+  }
+});
